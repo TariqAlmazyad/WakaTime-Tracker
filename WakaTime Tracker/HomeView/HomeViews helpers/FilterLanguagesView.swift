976380b9a2eee4f4dd9044968b9f44c28e4f0a32
Tariq Almazyad <tariq.almazyad@gmail.com>
2020-12-17 23:32:48 +0300

    version 1.4

--- WakaTime Tracker/HomeView/HomeViews helpers/FilterLanguagesView.swift
+++ WakaTime Tracker/HomeView/HomeViews helpers/FilterLanguagesView.swift
@@ -19,34 +19,43 @@ struct FilterLanguagesView: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack {
                     ForEach(LanguageSelection.allCases, id:\.self) { language in
-                        Button(action: {
-                            withAnimation{
-                                selectedLanguage = language
-                            }
-                        }, label: {
-                            VStack {
-                                Text(language.languageName)
-                                    .font(.subheadline)
-                                    .foregroundColor(selectedLanguage == language ? Color(#colorLiteral(red: 0.9019607843, green: 0.9019607843, blue: 0.9019607843, alpha: 1)) : Color(#colorLiteral(red: 0.4705882353, green: 0.4705882353, blue: 0.4705882353, alpha: 1)))
-                                    .frame(width: widthForTab(language.rawValue) + 40, height: 20)
-                                Rectangle()
-                                    .foregroundColor(selectedLanguage == language ? Color(#colorLiteral(red: 0.9019607843, green: 0.9019607843, blue: 0.9019607843, alpha: 1)) : .clear)
-                                    .frame(width: widthForTab(language.rawValue), height: 3)
-                            }
-                        })
+                        FilterCellView(selectedLanguage: $selectedLanguage, language: language )
                     }
                 }.padding(.horizontal)
             }
         }
     }
-    fileprivate func widthForTab(_ text: String) -> CGFloat {
-        return text.widthOfString(usingFont: .systemFont(ofSize: 16, weight: .bold))
-    }
+    
 }
 
 struct FilterLanguagesView_Previews: PreviewProvider {
     static var previews: some View {
         HomeVGridView()
             .environmentObject(neumorphism)
+    }
+}
+
+struct FilterCellView: View {
+    @Binding var selectedLanguage: LanguageSelection
+    let language: LanguageSelection
+    var body: some View {
+        Button(action: {
+            withAnimation{
+                selectedLanguage = language
+            }
+        }, label: {
+            VStack {
+                Text(language.languageName)
+                    .font(.subheadline)
+                    .foregroundColor(selectedLanguage == language ? Color(#colorLiteral(red: 0.9019607843, green: 0.9019607843, blue: 0.9019607843, alpha: 1)) : Color(#colorLiteral(red: 0.4705882353, green: 0.4705882353, blue: 0.4705882353, alpha: 1)))
+                    .frame(width: widthForTab(language.rawValue) + 40, height: 20)
+                Rectangle()
+                    .foregroundColor(selectedLanguage == language ? Color(#colorLiteral(red: 0.9019607843, green: 0.9019607843, blue: 0.9019607843, alpha: 1)) : .clear)
+                    .frame(width: widthForTab(language.rawValue), height: 3)
+            }
+        })
+    }
+    fileprivate func widthForTab(_ text: String) -> CGFloat {
+        return text.widthOfString(usingFont: .systemFont(ofSize: 16, weight: .bold))
     }
 }
